10fbfaf8a9a7036de8a2c66cb8d70b58c86e5e10
anandpandeylive12 <anandpandeylive12@gmail.com>
2025-10-02 00:55:25 +0530

    initial commit

---
+++ app/_lib/private-folder.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const privatefolder = () => {
+  return (
+    <div>
+        <h1 className='text-bold text-red-700'>
+            you can not view this in the Routing becsause it is in the private folder. 
+        </h1>
+    </div>
+  )
+}
+
+export default privatefolder
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-bold'>page</div>
+  )
+}
+
+export default page
